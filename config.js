821cd742ffdc43f3cbfacab74a4750ebceb69ec3
Mamo Tesema <mamo.t@cheq.ai>
2022-09-18 17:36:03 +0300

    remove captch middleware

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-const recaptcha = require('./helpers/captcha');
-
 module.exports = {
 	apiKey: process.env.CHEQ_API_KEY,
 	tagHash: process.env.CHEQ_TAG_HASH,
-	callback: recaptcha.middleware,
+	callback: function (req, res, next){
+		res.redirect('https://www.youtube.com/watch?v=LButXcZ57pc')
+	},
 	redirectUrl: 'https://invalid-user.com',
 	apiEndpoint: 'https://rti-us-east-1.cheqzone.com',
 	trustedIPHeader: 'Client-Ip',
@@ -11,8 +11,8 @@ module.exports = {
 	timeout: 1000,
 	URIExclusion: ['/about', /\/add_to_cart.*item=698/],
 	threatTypesCodes: {
-		blockRedirect: [2, 3, 6, 7, 9, 13],
-		captcha: [4, 5]
+		blockRedirect: [2, 3, 6, 7, 9],
+		captcha: [4, 5, 13]
 	},
 	getResourceType: function(req) {
 		if(req.method === 'POST') {
